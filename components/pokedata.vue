d62f4c567651a87eefb7613d824951866c4204d6
Selman Ciftci <selman.ciftci.coding@gmail.com>
2024-03-09 00:15:51 +0100

    fetching one pokemon works

---
+++ components/pokedata.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+const URL = 'https://pokeapi.co/api/v2/pokemon/ditto'
+
+const { data } = await useFetch(URL)
+</script>
+
+<template>
+    <div>Some data: {{ (data as any).name }}</div>
+</template>
+
+<style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,11 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div>
-    <h1>Nuxt Routing set up successfully!</h1>
-    <a href="https://nuxt.com/docs/getting-started/routing" target="_blank">Learn more about Nuxt Routing</a>
-  </div>
+    <div>
+        <h1>Nuxt Routing set up successfully!</h1>
+        <a href="https://nuxt.com/docs/getting-started/routing" target="_blank"
+            >Learn more about Nuxt Routing</a
+        >
+        <pokedata />
+    </div>
 </template>
